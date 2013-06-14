72e65d3e8d8f3e8520264a6950470c01cfc98258
KITAO Kaoru <kero@MacMini.local>
2013-06-14 21:29:21 +0900

    戻した

--- js/windowResize.js
+++ js/windowResize.js
@@ -2,12 +2,18 @@ $(document).ready(function(){
 	var resize = function(){
 		var windowWidth = parseInt( $(window).width(), 10 );
 		var windowHeight = parseInt( $(window).height(), 10 );
+		var menuHeight = 100;
 		$( '#gmap' ).css({
 							'width' : '100%',
-							'height' : '100%'
+							'height' : String(windowHeight - menuHeight) + 'px'
 						});
+		if ( windowWidth + windowHeight <= 1000 ) {
+			$( 'img' ).css({ 'width' : '31px', 'height' : '31px', 'margin-right' : '15px' });
+			$( 'li.buttons' ).css({ 'clear' : 'both', 'padding-top' : '20px' });
+		} else {
+			$( 'img' ).css({ 'width' : '62px', 'height' : '62px' });
+		}
 	};
-	
 	resize();
 	$( window ).resize( function(){
 		resize();
